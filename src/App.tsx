5b07b204e6c22ce25cd52ecf660e1e85dae00d88
a-sakhizada <arezousakhizada@gmail.com>
2021-11-11 00:43:15 -0500

    body highlighting component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import BodyParts from './Components/BodyParts';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <BodyParts/>
     </div>
   );
 }
---
+++ src/Components/BodyParts.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Model, { IExerciseData, IMuscleStats } from 'react-body-highlighter';
+
+export default function BodyParts() {
+  const data: IExerciseData[] = [
+    { name: 'Bench Press', muscles: ['chest', 'triceps', 'front-deltoids'] },
+    { name: 'Push Ups', muscles: ['chest'] },
+  ];
+  
+  const handleClick = React.useCallback(({ muscle, data }: IMuscleStats) => {
+    const { exercises, frequency } = data;
+
+    alert(`You clicked the ${muscle}! You've worked out this muscle ${frequency} times through the following exercises: ${JSON.stringify(exercises)}`)
+
+  }, [data]);
+
+  return (
+    <Model
+      data={data}
+      style={{ width: '20rem', padding: '5rem' }}
+      onClick={handleClick}
+    />
+  );
+}
