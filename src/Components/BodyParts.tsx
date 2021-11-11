2081b5d6b7e25e91d752ab9678f4f50ca7d46406
Brynn Williams <williamsbrynn@gmail.com>
2021-11-11 14:51:46 -0800

    Add Msucles.js response on tricep click

--- src/Components/BodyParts.tsx
+++ src/Components/BodyParts.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Muscles from './Muscles';
 import Model, { IExerciseData, IMuscleStats } from 'react-body-highlighter';
+
+//Link to npm for react-body-highlighter: https://www.npmjs.com/package/react-body-highlighter //
 
 export default function BodyParts() {
   const data: IExerciseData[] = [
-    { name: 'Bench Press', muscles: ['chest', 'triceps', 'front-deltoids'] },
-    { name: 'Push Ups', muscles: ['chest'] },
+    { name: 'Bench Press', muscles: ['triceps'] },
   ];
-  
-  const handleClick = React.useCallback(({ muscle, data }: IMuscleStats) => {
-    const { exercises, frequency } = data;
+  const [clickableMuscle, setClickableMuscle] = useState(``);
 
-    alert(`You clicked the ${muscle}! You've worked out this muscle ${frequency} times through the following exercises: ${JSON.stringify(exercises)}`)
+  const handleClick = React.useCallback(({ muscle}: IMuscleStats) => {
+    setClickableMuscle(`${muscle}`);
+    alert(`You clicked the ${muscle}! Scroll down to learn more!`)
 
   }, [data]);
 
+if (clickableMuscle === `triceps`) {
   return (
+  <div className='BodyParts'>
     <div className='ClickableBody'>
       <Model
         data={data}
         style={{ width: '30rem' }}
         onClick={handleClick}
       />
-    </div>
-  );
+      <br/>
+      <Muscles props={clickableMuscle} />
+   </div>
+  </div>) } else {
+      return (
+        <div className='BodyParts'>
+          <div className='ClickableBody'>
+            <Model
+              data={data}
+              style={{ width: '30rem' }}
+              onClick={handleClick}
+            />
+         </div>
+        </div>);
+  }
+
+  
 }
---
+++ src/Components/Muscles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Muscles (props) {
+    console.log(props)
+    
+    return (
+        <div className='Muscles'>
+            <h1>Hello from triceps!</h1>
+        </div>
+    );
+}
